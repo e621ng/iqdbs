6279d4f5d38e09814a513259d58d669e6e085af5
Earlopain <earlopain@protonmail.com>
2022-12-28 19:01:21 +0100

    Remove unused env variables

--- web/iqdbs.rb
+++ web/iqdbs.rb
@@ -13,12 +13,6 @@ require "iqdb/command"
 
 set :port, ENV["SINATRA_PORT"]
 
-# before "/similar" do
-#   if params["key"] != ENV["AUTH_KEY"]
-#     halt 401
-#   end
-# end
-
 def find_referer(url)
   if url =~  /\Ahttps?:\/\/(?:\w+\.)?pixiv\.net/ || url =~ /\Ahttps?:\/\/i\.pximg\.net/
     return "https://www.pixiv.net"
